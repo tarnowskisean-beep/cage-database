4e5e98b304e2c5844c65dd9ea1d6d7eba207fa1d
Sean Tarnowski <seantarnowski@Mac.lan>
2026-01-02 13:49:07 -0800

    style: Switch to Greyscale Professional Theme

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -54,7 +54,7 @@ export default function Sidebar() {
                 }}>
                     <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <circle cx="24" cy="24" r="23" stroke="white" strokeWidth="1.5" />
-                        <path d="M24 8L27 21L40 24L27 27L24 40L21 27L8 24L21 21L24 8Z" fill="#cbd5e1" />
+                        <path d="M24 8L27 21L40 24L27 27L24 40L21 27L8 24L21 21L24 8Z" fill="white" />
                     </svg>
                 </div>
                 {!isCollapsed && (
