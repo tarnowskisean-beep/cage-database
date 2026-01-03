53a62383d6e3a149b50aec1de98a3343856714c9
Sean Tarnowski <seantarnowski@Mac.lan>
2026-01-02 23:26:31 -0800

    chore: temporary disable middleware to debug vercel

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { withAuth } from "next-auth/middleware"
-
-export default withAuth({
-    pages: {
-        signIn: "/login",
-    },
-})
-
-export const config = { matcher: ["/((?!api/auth|login|_next/static|_next/image|favicon.ico).*)"] }
