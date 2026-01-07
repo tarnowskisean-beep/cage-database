bffc7e8c8136640e18d5b46518c7200e3e997340
Sean Tarnowski <seantarnowski@Mac.lan>
2026-01-07 01:28:27 -0800

    fix: add missing sql placeholder in save-donation api

--- app/api/save-donation/route.ts
+++ app/api/save-donation/route.ts
@@ -75,7 +75,7 @@ export async function POST(request: Request) {
              "ReceiptYear", "ReceiptQuarter", "IsInactive", "Comment",
              "CampaignID", "ResolutionStatus"
             )
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, NOW(), $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, NOW(), $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36, $37)
             RETURNING *`,
             [
                 batch.ClientID,
